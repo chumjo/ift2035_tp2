bcb0d0c881e54048e526d1a53b847b31661f4dbd
Jonathan Larose <jonathan.larose@umontreal.ca>
2017-07-15 11:44:55 -0400

    LearnC test de comprehension

--- Code/learnC.c
+++ Code/learnC.c
@@ -6,19 +6,18 @@ int main()
 
     struct bloc
     {
-        void *addr;
-        //size_t size;
+        size_t size;
         int free;
-        struct bloc *next;
+        struct bloc *next, *pred;
     };
 
     int tm = 1876870;
     int* dix = &tm;
 
     //struct bloc bloc1 = {*dix, sizeof(1024), false, NULL};
-    tototo bloc1 = {*dix, NULL};
-    struct bloc* bloc3 = {*dix, NULL};
-    toto bloc2 = {*dix, NULL};
+    tototo bloc1 = {*dix, NULL, NULL};
+    struct bloc* bloc3 = {*dix, NULL, NULL};
+    toto bloc2 = {*dix, NULL, NULL};
     //struct bloc bloc2 = {*dix, sizeof(1024), false, bloc2};
 
     printf("The size of a bloc is %i\n", sizeof(bloc1));
