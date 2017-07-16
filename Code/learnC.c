4ac99d2eca1afa4ac5aa659e00953d9dcb4b7666
Jonathan-PC\Jonathan <jonathan.larose@umontreal.ca>
2017-07-16 14:59:10 -0400

    Auto stash before merge of "master" and "origin/master"
    Liste Global Static, find()

--- Code/learnC.c
+++ Code/learnC.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "mymalloc.h"
 
 
 int main()
 {
-    typedef struct bloc *tototo;
-    typedef struct bloc toto;
+    typedef struct bloc *newBloc;
+    typedef struct bloc bloc;
+
+    //newBloc *find;
 
     struct bloc
-    {
-        size_t size;
-        int free;
-        struct bloc *next, *pred;
-    };
+        {
+            size_t size;
+            int free;
+            struct bloc *next, *pred;
+        };
 
     int tm = 1876870;
-    int* dix = &tm;
+    int *dix = &tm;
     int page = 4096;
 
 
     //struct bloc bloc1 = {*dix, sizeof(1024), false, NULL};
-    tototo bloc1 = {*dix, NULL, NULL};
-    struct bloc* bloc3 = {*dix, NULL, NULL};
-    toto bloc2 = {*dix, NULL, NULL};
+    newBloc bloc1 = malloc(sizeof(*bloc1));
+    bloc1->size = 1000;
+    bloc1->free = 0;
+
+    newBloc bloc2 = malloc(sizeof(*bloc2));
+    newBloc bloc3 = malloc(sizeof(*bloc3));
+
+
     //struct bloc bloc2 = {*dix, sizeof(1024), false, bloc2};
 
     printf("The size of a bloc is %i\n", sizeof(bloc1));
-    printf("The size of a bloc is %i\n", sizeof(bloc3));
     printf("The size of a bloc is %i\n", sizeof(bloc2));
+    printf("The size of a bloc is %i\n", sizeof(bloc3));
     printf("The size of a bloc is %i\n", sizeof(struct bloc));
 
     printf("page - 2*bloc : %i\n", page-(2*sizeof(bloc2)));
 
-    int *addr = mymalloc(sizeof(8000));
+    //int *addr = mymalloc(8000);
     int *addr2 = mymalloc(sizeof(int));
 
     //char *src = mmap(0, sizeof(bloc2), PROT_READ, MAP_SHARED, -1, 0);
--- Code/mymalloc.c
+++ Code/mymalloc.c
@@ -4,34 +4,69 @@
 #include "mymalloc.h"
 #include <stdbool.h>
 #include <stddef.h>
-#include <sys/mman.h>
+#include <stdio.h>
+#include <stdlib.h>
+//#include <sys/mman.h>
 
 //Macros
-#define BLOC_SIZE sizeof(struct bloc)
-#define PAGE 4096
+#define BLOC_SIZE (sizeof(struct bloc))
+#define PAGE (4096)
 
 
-typedef struct bloc *newBloc;
-
-
-struct bloc
+typedef struct bloc
     {
         size_t size;
         int free;
         struct bloc *next, *pred;
-    };
+    } *Bloc;
 
 
-newBloc memoryList;
+Bloc blocList(){
 
-newBloc find(size_t size){
+    printf("On retrieve la liste!\n");
 
-    newBloc current = memoryList;
+    static Bloc memoryList = NULL;
+
+    if(memoryList == NULL){
+
+        printf("Creation du premier bloc pour la liste de bloc!\n");
+
+        memoryList = malloc(sizeof(*memoryList));
+        memoryList->size = PAGE-BLOC_SIZE;
+        memoryList->free = 1;
+        memoryList->next = NULL;
+        memoryList->pred = NULL;
+
+        printf("memoryList -> size :%i\n", memoryList->size);
+        printf("memoryList -> free :%i\n", memoryList->free);
+        printf("memoryList -> next :%i\n", memoryList->next);
+        printf("memoryList -> pred :%i\n", memoryList->pred);
+    }
+
+    printf("C'est un succes!!! :-)\n");
+
+    return memoryList;
+}
+
+Bloc find(size_t size){
+
+    printf("On trouve un espace dans la memoire qu'on possede deja! \n");
+
+    Bloc current = blocList();
+
+    printf("memoryList -> size :%i\n", current->size);
+    printf("memoryList -> free :%i\n", current->free);
+    printf("memoryList -> next :%i\n", current->next);
+    printf("memoryList -> pred :%i\n", current->pred);
 
     while(current != NULL){
 
-        if(current->free == 1 && current->size < size+BLOC_SIZE)
+        printf("Je cherches...\n");
+
+        if((current->free == 1) && (current->size > (size + BLOC_SIZE))){
+            printf("OMG!!! J'ai trouve!!!!\n");
             break;
+        }
         else
             current = current->next;
     }
@@ -41,16 +76,21 @@ newBloc find(size_t size){
 
 
 void *mymalloc(size_t size){
+
+    printf("I am in mymalloc! \n");
+
     // À modifier
     void *addr;
 
-
     //Si on a besoin d'une nouvelle page mémoire
-    if(size > PAGE-2*BLOC_SIZE){
+    if(size > (PAGE-2*BLOC_SIZE)){
         printf("Size bigger than a page. Custom size page : %s\n", size);
     	//addr = mmap(size);
         return NULL;
     }
+    else
+        printf("Taille respectable, on continue!\n");
+
     addr = find(size);
 
     if(addr != NULL){
