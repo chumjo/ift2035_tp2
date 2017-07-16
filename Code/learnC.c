e4347d42ccd40d119b2082534b609bb3d537582a
chumjo <jonathan.larose.fu@gmail.com>
2017-07-16 10:44:06 -0400

    Mise en place linux....

--- Code/learnC.c
+++ Code/learnC.c
@@ -1,4 +1,6 @@
- #include <stdio.h>
+#include <stdio.h>
+#include <sys/mman.h>
+
 int main()
 {
     typedef struct bloc *tototo;
@@ -14,6 +16,8 @@ int main()
     int tm = 1876870;
     int* dix = &tm;
 
+
+
     //struct bloc bloc1 = {*dix, sizeof(1024), false, NULL};
     tototo bloc1 = {*dix, NULL, NULL};
     struct bloc* bloc3 = {*dix, NULL, NULL};
@@ -23,6 +27,8 @@ int main()
     printf("The size of a bloc is %i\n", sizeof(bloc1));
     printf("The size of a bloc is %i\n", sizeof(bloc3));
     printf("The size of a bloc is %i\n", sizeof(bloc2));
+
+    char *src = mmap(0, sizeof(bloc2), PROT_READ, MAP_SHARED, -1, 0);
 
     //printf("Sorry, can't talk now.");
     //printf("I'm busy!");
